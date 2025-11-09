feb78254afef63f5a29781475e3fc5d2d0ba263d
Valfran <valfran-goncalves@hotmail.com>
2025-11-09 18:20:30 -0300

    feat: adicionando background image na home e inserindo css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css";
-import Header from "./components/Header";
+import Header from "./components/Header/Header.jsx";
 function App() {
   return (
     <>
       <Header />
+      <div className="showcase_container">
+        <h1>
+          Oi, eu sou o <br />
+          Valfran Jr!
+        </h1>
+        <p>Um desenvolvedor web, apaixonado por tecnologia e inovação.</p>
+      </div>
     </>
   );
 }
