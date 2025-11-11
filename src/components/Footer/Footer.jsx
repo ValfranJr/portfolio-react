3228988a95902ea86da3d7b3d188cfb74f82cb3a
Valfran <valfran-goncalves@hotmail.com>
2025-11-11 20:30:16 -0300

    feat:  atulizando componente footer inserindo icones email,github,linkedin

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,18 @@
 import styles from "./Footer.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 export default function Footer() {
   return (
     <div className={styles.footer_container}>
       <div className={styles.social_itens}>
-        <a href="mailto:valfran-goncalves@hotmail.com"></a>
-        <a href="https://www.linkedin.com/in/valfran-goncalves/"></a>
+        <a href="mailto:valfran-goncalves@hotmail.com">
+          <FontAwesomeIcon icon={faEnvelope} size="2x" />
+        </a>
+        <a href="https://www.linkedin.com/in/valfran-goncalves/">
+          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        </a>
         <a href="https://github.com/ValfranJr">
           <FontAwesomeIcon icon={faGithub} size="2x" />
         </a>
