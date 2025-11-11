ef8bcd8702365b79a3cea6ed5e514df8df10aaec
Valfran <valfran-goncalves@hotmail.com>
2025-11-11 07:39:20 -0300

    refactor: alterando icones e fotos da pasta public para a pasta assets e importando para um arquivo index.js

---
+++ src/assets/index.js
@@ -0,0 +1,23 @@
+import HTMLICON from "./HTML ICON.png";
+import CSSICON from "./CSS ICON.png";
+import JSICON from "./JS ICON.png";
+import GITICON from "./git_icon.png";
+import GITHUBICON from "./github_icon.png";
+import REACTICON from "./React-icon.svg.png";
+import NEXTJSICON from "./nextjs_icon.png";
+import TYPESCRIPTICON from "./typescript.png";
+import GITJESTICICON from "./jest_icon.png";
+import TESTINGLIBRARYICON from "./testing-library.png";
+
+export {
+  HTMLICON,
+  CSSICON,
+  JSICON,
+  GITICON,
+  GITHUBICON,
+  REACTICON,
+  NEXTJSICON,
+  TYPESCRIPTICON,
+  GITJESTICICON,
+  TESTINGLIBRARYICON,
+};
